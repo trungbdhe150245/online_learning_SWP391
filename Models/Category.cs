ddf54264439b95c5a68db394a2d75dae0946a3ac
tiendat91 <datdthe151528@fpt.edu.vn>
2022-01-19 17:58:15 +0700

    Add models and layout for home page

---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SWP391_OnlineLearning_Platform.Models
+{
+    public class Category
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(255, ErrorMessage = "Not exceed 255")]
+        public string Value { get; set; }
+    }
+}
---
+++ Models/User_Question_Answer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SWP391_OnlineLearning_Platform.Models
+{
+    public class User_Question_Answer
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(255, ErrorMessage = "Not exceed 255")]
+        public string User_Choice { get; set; }
+        [DisplayName("Question_Bank")]
+        public int Question_Id { get; set; }
+        [DisplayName("User_Quiz")]
+        public int User_Quiz_Id { get; set; }
+        [ForeignKey("Question_Id")]
+        public virtual Question_Bank Question_Bank { get; set; }
+        [ForeignKey("User_Quiz_Id")]
+        public virtual User_Quiz User_Quiz { get; set; }
+
+    }
+}
