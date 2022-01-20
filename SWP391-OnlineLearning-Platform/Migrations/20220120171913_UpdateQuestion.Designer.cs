ab1e9b910e7d6ae08c5bbfcabf2c50e6f4814c12
TruongLam2905 <truonglam29052001@gmail.com>
2022-01-21 00:20:24 +0700

    UpdateQuest

--- SWP391-OnlineLearning-Platform/Migrations/20220120171913_UpdateQuestion.Designer.cs
+++ SWP391-OnlineLearning-Platform/Migrations/20220120171913_UpdateQuestion.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SWP391_OnlineLearning_Platform.Data;
 
 namespace SWP391_OnlineLearning_Platform.Migrations
 {
     [DbContext(typeof(OnlineLearningDbContext))]
-    partial class OnlineLearningDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220120171913_UpdateQuestion")]
+    partial class UpdateQuestion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
