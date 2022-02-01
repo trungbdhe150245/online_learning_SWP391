676733283c2036a4e47a8aba4ecfa0d9694971ea
tiendat91 <datdthe151528@fpt.edu.vn>
2022-02-02 00:31:55 +0700

    Add pagination using NuGetPackage

--- Controllers/PublicFeatureController.cs
+++ Controllers/PublicFeatureController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Diagnostics.Tracing.Parsers.Kernel;
+using PagedList.Core;
+using SWP391_OnlineLearning_Platform.Data;
+using SWP391_OnlineLearning_Platform.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +12,29 @@ namespace SWP391_OnlineLearning_Platform.Controllers
 {
     public class PublicFeatureController : Controller
     {
+        private readonly OnlineLearningDbContext _db;
+
+        public PublicFeatureController(OnlineLearningDbContext db)
+        {
+            _db = db;
+        }
+
+        //PHÂN TRANG
+        public IActionResult Index(int? page)
+        {
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pageSize = 20;
+            var IsBlogs = _db.Blogs.OrderByDescending(x => x.Id);
+            PagedList<Blog> models = new PagedList<Blog>(IsBlogs, pageNumber, pageSize);
+            return View(models);
+
+        }
         public IActionResult BlogList()
         {
+            
             return View();
         }
+
         public IActionResult BlogDetail()
         {
             return View();
