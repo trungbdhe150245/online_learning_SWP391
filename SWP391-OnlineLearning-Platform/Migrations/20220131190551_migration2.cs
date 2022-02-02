03ec782a30f91dd42b429a3f6e5992d9326cffa1
trungbdhe150245 <trungbdhe150245@fpt.edu.vn>
2022-02-02 12:52:10 +0700

    update style

---
+++ SWP391-OnlineLearning-Platform/Migrations/20220131190551_migration2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SWP391_OnlineLearning_Platform.Migrations.OnlineLearningDb
+{
+    public partial class migration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
