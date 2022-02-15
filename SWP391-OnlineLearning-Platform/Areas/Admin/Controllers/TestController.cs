6c637b06c7f1491b24ee51a34f49523e508c781f
tiendat91 <datdthe151528@fpt.edu.vn>
2022-02-15 23:37:33 +0700

    Add Question List

---
+++ SWP391-OnlineLearning-Platform/Areas/Admin/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SWP391_OnlineLearning_Platform.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SWP391_OnlineLearning_Platform.Areas.Admin.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly OnlineLearningDbContext _db;
+
+        public TestController(OnlineLearningDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult QuestionList()
+        {
+            return View();
+        }
+    }
+}
