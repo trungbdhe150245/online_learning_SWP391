3a0a41d850d175a372817c41b7895b18b2f2bc22
trungbdhe150245 <trungbdhe150245@fpt.edu.vn>
2022-02-18 01:29:08 +0700

    using braintree sandbox

---
+++ SWP391-OnlineLearning-Platform/Utility/Services/IBraintreeService.cs
@@ -0,0 +1,14 @@
+﻿using Braintree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SWP391_OnlineLearning_Platform.Utility.Services
+{
+    public interface IBraintreeService
+    {
+        IBraintreeGateway CreateGateway();
+        IBraintreeGateway GetGateway();
+    }
+}
