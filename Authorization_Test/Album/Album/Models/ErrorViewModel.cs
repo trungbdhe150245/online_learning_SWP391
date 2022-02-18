b662b1ad266f93f353dacbda2735f33dd95be755
trungbdhe150245 <trungbdhe150245@fpt.edu.vn>
2022-02-18 14:39:32 +0700

    authorization test

---
+++ Authorization_Test/Album/Album/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Album.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
