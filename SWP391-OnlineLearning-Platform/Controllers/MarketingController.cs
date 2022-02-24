784d01de74e5a33421ce2eb2dafc678b75fefd9e
TruongLam2905 <truonglam29052001@gmail.com>
2022-02-25 00:27:45 +0700

    Up PostDetail

---
+++ SWP391-OnlineLearning-Platform/Controllers/MarketingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SWP391_OnlineLearning_Platform.Controllers
+{
+    public class MarketingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
