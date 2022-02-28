5406a7cc0ed77be91f03e6f3b6efeaf5c16a25f5
tiendat91 <datdthe151528@fpt.edu.vn>
2022-02-28 16:31:41 +0700

    paging quizs list

--- SWP391-OnlineLearning-Platform/Areas/Admin/Controllers/QuizsController.cs
+++ SWP391-OnlineLearning-Platform/Areas/Admin/Controllers/QuizsController.cs
@@ -22,12 +22,37 @@ namespace SWP391_OnlineLearning_Platform.Areas.Admin.Controllers
         }
 
         // GET: Admin/Quizs
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string key, int page)
         {
-            var onlineLearningDbContext = _context.Quizzes.Include(q => q.Course).Include(q => q.Quiz_Level).Include(q => q.Quiz_Type);
-            return View(await onlineLearningDbContext.ToListAsync());
+            return View(Filter(key,page));
 
         }
+        public IEnumerable<Quiz> Filter(string key, int page)
+        {
+            
+            IEnumerable<Quiz> quizs = _context.Quizzes.Include(q => q.Course).Include(q => q.Quiz_Level).Include(q => q.Quiz_Type);
+
+            if (!string.IsNullOrEmpty(key))
+            {
+                quizs = quizs.Where(s => s.Name!.ToLower().Contains(key.ToLower()));
+            }
+            
+
+            const int pageSize = 1;
+            if (page < 1)
+            {
+                page = 1;
+            }
+
+            int resCount = quizs.Count();
+            var pager = new Paginated(resCount, page, pageSize);
+            int recSkip = (page - 1) * pageSize;
+
+            var data = quizs.Skip(recSkip).Take(pager.PageSize);
+            this.ViewBag.Paginated = pager;
+            return data;
+        }
+
 
         // GET: Admin/Quizs/Details/5
         public async Task<IActionResult> Details(int? id)
