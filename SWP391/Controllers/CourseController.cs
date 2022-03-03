3250037b86f9d3589ce4e790246be9e4f5c150a9
trungbdhe150245 <trungbdhe150245@fpt.edu.vn>
2022-03-03 20:53:52 +0700

    helpcenter1

---
+++ SWP391/Controllers/CourseController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SWP391.Data;
+using System.Collections.Generic;
+using System.Linq;
+namespace SWP391.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly LearningDbContext _db;
+        public CourseController(LearningDbContext db)
+        {
+            _db = db; 
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Lesson(int? id)
+        {
+            return Ok(id);
+        }
+        public IActionResult Content() 
+        {
+
+            return Ok();
+        }
+
+
+    }
+}
