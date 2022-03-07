061d4abded35a45fa7d0c657285822d46c372a17
TruongLam2905 <truonglam29052001@gmail.com>
2022-03-07 22:08:47 +0700

    DesignCourseApprove

--- SWP391/Controllers/Admin/DashBoardController.cs
+++ SWP391/Controllers/Admin/DashBoardController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SWP391.Data;
 using SWP391.Models;
+using SWP391.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,7 @@ using System.Threading.Tasks;
 
 namespace SWP391.Controllers.Admin
 {
+    [ServiceFilter(typeof(Filter))]
     public class DashBoardController : Controller
     {
         private readonly LearningDbContext _db;
@@ -25,33 +27,32 @@ namespace SWP391.Controllers.Admin
 
         public IActionResult DashBoard()
         {
-            var user = HttpContext.User;
-            if(!user.Identity.IsAuthenticated)
-            {
-                return Redirect($"~/Home/Index");
-            }
-            var obj = _userManager.GetUserAsync(user);
-            this.ViewBag.User = obj.Result;
+            //var user = HttpContext.User;
+            //if(!user.Identity.IsAuthenticated)
+            //{
+            //    return Redirect($"~/login");
+            //}
+            //var obj = _userManager.GetUserAsync(user);
+            //this.ViewBag.User = obj.Result;
+            var obj = GetCurrentUser();
             return View(obj);
         }
 
         
-        public async Task<IActionResult> GetProfile()
-        {
-            var user = HttpContext.User;
-            if (!user.Identity.IsAuthenticated)
-            {
-                return Redirect($"~/Home/Index");
-            }
-            var obj = _userManager.GetUserAsync(user);
-
-
-            return new OkObjectResult(obj);
-        }
-
         public IActionResult RegistrationList()
         {
-            return View();
+            var obj = GetCurrentUser();
+            IEnumerable<Course> list = _db.Courses.Where(c => c.StatusId == "3");
+            ViewBag.CourseRegis = list;
+            return View(list);
+        }
+
+        public Task<AppUser> GetCurrentUser()
+        {
+            var user = HttpContext.User;
+            var obj = _userManager.GetUserAsync(user);
+            this.ViewBag.User = obj.Result;
+            return obj;
         }
     }
 }
