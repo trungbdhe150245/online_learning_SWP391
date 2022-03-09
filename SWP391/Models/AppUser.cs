7c3a0d88d962f24b97e6240f555677f81f3b4072
Bùi Đức Trung <trungbdhe150245@fpt.edu.vn>
2022-03-09 13:48:56 +0700

    Fix Chatbot layout

--- SWP391/Models/AppUser.cs
+++ SWP391/Models/AppUser.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SWP391.Models
