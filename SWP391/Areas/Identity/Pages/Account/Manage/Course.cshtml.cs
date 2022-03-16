ed4a954e2126037fdf6ff2cd5ca9cdb5d4c640ca
TruongLam2905 <truonglam29052001@gmail.com>
2022-03-16 09:55:31 +0700

    UpMyCourse

--- SWP391/Areas/Identity/Pages/Account/Manage/Course.cshtml.cs
+++ SWP391/Areas/Identity/Pages/Account/Manage/Course.cshtml.cs
@@ -27,74 +27,41 @@ namespace SWP391.Areas.Identity.Pages.Account.Manage
         }
 
         public Course Course { get; set; }
-        
+
 
 
         //LẤY THÔNG TIN CỦA USER
-        private async Task LoadAsync(AppUser user)
-        {
-            var userName = await _userManager.GetUserNameAsync(user);
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            Username = userName;
-            FullName = user.FullName;
-            ProfilePictureURL = user.ProfilePictureURL;
+        //private async Task LoadAsync(AppUser user)
+        //{
+        //    var userName = await _userManager.GetUserNameAsync(user);
+        //    var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+        //    Username = userName;
+        //    FullName = user.FullName;
+        //    ProfilePictureURL = user.ProfilePictureURL;
 
 
-            Input = new InputModel
-            {
-                PhoneNumber = phoneNumber,
-                Address = user.Address,
-                FullName = user.FullName,
-                ProfilePictureURL = user.ProfilePictureURL,
-            };
-        }
+        //    Input = new InputModel
+        //    {
+        //        PhoneNumber = phoneNumber,
+        //        Address = user.Address,
+        //        FullName = user.FullName,
+        //        ProfilePictureURL = user.ProfilePictureURL,
+        //    };
+        //}
 
         //KIỂM TRA USER ĐÃ LOGIN HAY CHƯA?
-        public async Task<IActionResult> OnGetAsync()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
+        //public async Task<IActionResult> OnGetAsync()
+        //{
+        //    var user = await _userManager.GetUserAsync(User);
+        //    if (user == null)
+        //    {
+        //        return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+        //    }
 
-            await LoadAsync(user);
-            return Page();
-        }
+        //    await LoadAsync(user);
+        //    return Page();
+        //}
 
-        public async Task<IActionResult> OnPostAsync()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
 
-            if (!ModelState.IsValid)
-            {
-                await LoadAsync(user);
-                return Page();
-            }
-
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            if (Input.PhoneNumber != phoneNumber)
-            {
-                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
-                if (!setPhoneResult.Succeeded)
-                {
-                    StatusMessage = "Unexpected error when trying to set phone number.";
-                    return RedirectToPage();
-                }
-            }
-
-            user.Address = Input.Address;
-            user.FullName = Input.FullName;
-            user.ProfilePictureURL = Input.ProfilePictureURL;
-
-            await _userManager.UpdateAsync(user);
-            await _signInManager.RefreshSignInAsync(user);
-            StatusMessage = "Your profile has been updated";
-            return RedirectToPage();
-        }
     }
 }
